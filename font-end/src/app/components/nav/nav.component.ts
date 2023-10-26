86656c3627eb6ca57201560a118a2c1b4b2dc72c
igorcamposdeborba <igor_campos5@hotmail.com>
2023-10-25 22:38:56 -0300

    created home

--- font-end/src/app/components/nav/nav.component.ts
+++ font-end/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,9 +9,9 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
-  constructor() {}
+  constructor(private router : Router) {}
 
   ngOnInit() : void {
-    
+    this.router.navigate(['home']);
   }
 }
